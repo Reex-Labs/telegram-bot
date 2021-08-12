9e17e01ee20d7e49f2956870f34cbf443704a130
ak <andreykrivtsov@gmail.com>
2021-08-13 02:55:36 +0300

    Added generate address function, get balance function and simple menu

--- src/address.js
+++
@@ -1,8 +0,0 @@
-import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
-
-export default async function genAddress() {
-    const wallet = await DirectSecp256k1HdWallet.generate(24, { prefix: "reex" })
-    const mnemonic = wallet.mnemonic
-    const [{ address }] = await wallet.getAccounts();
-    return { address, mnemonic }
-}
